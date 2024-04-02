0518e8d7d91414d553efe1a27e263044faabc04b
Anderson Marlon <anderson18.marlon@gmail.com>
2024-04-02 03:23:24 -0300

    Fix: Unicons import + Title

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -5,9 +5,6 @@ import './index.css'
 function App() {
     return (
         <>
-            <head>
-                <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css" />
-            </head>
             <Header />
             <AttendeeList />
         </>
