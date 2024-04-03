f38ca2bb3d7cf656519399972207be608adaa302
Anderson Marlon <anderson18.marlon@gmail.com>
2024-04-03 17:03:17 -0300

    Added: Third Day #Node + Bun Experimental

--- bun/src/error-handler.ts
+++ bun/src/error-handler.ts
@@ -4,7 +4,7 @@ import { ZodError } from "zod";
 
 type FastifyErrorHandler = FastifyInstance["errorHandler"];
 
-export const errorHandler: FastifyErrorHandler = (error, request, reply) => {
+export const errorHandler: FastifyErrorHandler = (error, _request, reply) => {
   if (error instanceof ZodError) {
     return reply.status(400).send({
       message: `Error during validation`,
